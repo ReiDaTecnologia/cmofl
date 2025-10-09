d99d3dcd0f3719f228fc183cec4490e9ddef9b48
Christian Henrique <30323772+ReiDaTecnologia@users.noreply.github.com>
2025-10-09 06:04:14 -0300

    Use SoundCategory instead of String
    Idk why I didn't do that since the beginning...

--- src/main/java/rei/cmofl/config/Config.java
+++ src/main/java/rei/cmofl/config/Config.java
@@ -7,17 +7,14 @@ import java.util.Objects;
 public class Config
 {
     public boolean enabled = true;
-    public Map<String, Integer> soundCategories = new HashMap<>();
+    public Map<SoundCategory, Integer> soundCategories = new HashMap<>();
 
     public Config()
     {
         for (SoundCategory soundCategory : SoundCategory.values())
         {
-            String key = soundCategory.getName();
-            Integer value = Objects.equals(key, SoundCategory.MUSIC.getName()) || Objects.equals(key, SoundCategory.RECORDS.getName()) ? 0 : -1;
-            if (Objects.equals(key, SoundCategory.RECORDS.getName()) || Objects.equals(key, SoundCategory.BLOCKS.getName()) || Objects.equals(key, SoundCategory.PLAYERS.getName()))
-                key = key + "s"; // For some reason their names are missing an "s" making SoundCategory.valueOf fail to find them
-            soundCategories.put(key, value);
+            Integer value = Objects.equals(soundCategory, SoundCategory.MUSIC) || Objects.equals(soundCategory, SoundCategory.RECORDS) ? 0 : -1;
+            soundCategories.put(soundCategory, value);
         }
     }
 }
