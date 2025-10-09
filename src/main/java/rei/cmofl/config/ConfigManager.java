d8bc809875567ca1d1c093f933359ed37b6b0d0f
Christian Henrique <30323772+ReiDaTecnologia@users.noreply.github.com>
2025-10-09 06:16:49 -0300

    Implement load exception handling
    I forgot to do that

--- src/main/java/rei/cmofl/config/ConfigManager.java
+++ src/main/java/rei/cmofl/config/ConfigManager.java
@@ -16,8 +16,18 @@ public class ConfigManager
     {
         if (configFile.exists())
         {
-            Toml toml = new Toml().read(configFile);
-            Config config1 = toml.to(Config.class);
+            Config config1 = null;
+            try
+            {
+                Toml toml = new Toml().read(configFile);
+                config1 = toml.to(Config.class);
+            }
+            catch (Exception exception)
+            {
+                ConfigurableMuteOnFocusLost.LOGGER.error(getTextString("cmofl.config.exception.load"), exception);
+                ConfigurableMuteOnFocusLost.LOGGER.warn(getTextString("cmofl.config.exception.default"));
+                resetConfigToDefault();
+            }
             if (config1 != null)
                 config = config1;
         }
@@ -40,18 +50,42 @@ public class ConfigManager
         catch (IOException ioException)
         {
             if (creatingConfig)
-                ConfigurableMuteOnFocusLost.LOGGER.error(Text.translatable("cmofl.config.exception.create").getString(), ioException);
+                ConfigurableMuteOnFocusLost.LOGGER.error(getTextString("cmofl.config.exception.create"), ioException);
             else
             {
                 ConfigurableMuteOnFocusLost.LOGGER.error(Text.translatable("cmofl.config.exception.save").getString(), ioException);
                 ConfigurableMuteOnFocusLost.LOGGER.warn(Text.translatable("cmofl.config.exception.default").getString());
-                config = new Config();
-                try
-                {
-                    writer.write(config, configFile);
-                }
-                catch (IOException ignored) {}
+                resetConfigToDefault();
             }
         }
+    }
+
+    private static void resetConfigToDefault()
+    {
+        TomlWriter writer = new TomlWriter();
+        config = new Config();
+        try
+        {
+            writer.write(config, configFile);
+        }
+        catch (IOException ignored) {}
+    }
+
+    private static String getTextString(String langKey) // For some reason, during mod initialization Text.translatable("langKey").getString() returns the langKey instead of the actual text based in the game's language, I couldn't find an alternative so hardcoded English it is...
+    {
+        String textString = Text.translatable(langKey).getString();
+        if (langKey.equals(textString))
+        {
+            switch (langKey)
+            {
+                case "cmofl.config.exception.default":
+                    return "Resetting config to default as a fix.";
+                case "cmofl.config.exception.create":
+                    return "An exception was been thrown while trying to create the config:";
+                case "cmofl.config.exception.load":
+                    return "An exception was been thrown while trying to load the config:";
+            }
+        }
+        return textString;
     }
 }
